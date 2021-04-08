8b72cc321698a8779a89277ba2eae3795229b020
Lina <ushakova.poly21@gmail.com>
2021-04-08 14:35:25 +0300

    ....

--- p/Website/web.py
+++ p/Website/web.py
@@ -11,22 +11,20 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 app.config["CACHE_TYPE"] = "null"
 cache.init_app(app)
-with app.app_context():
-    cache.clear()
 
 
 def main():
-    db_session.global_init("../Website/db/blogs.sqlite")
-    app.run(port=8000, host='127.0.0.1')
-    """product = Product()
+    """db_session.global_init("../Website/db/blogs.sqlite")
+    product = Product()
     product.title = "Лайтстик BTS"
     product.describtion = "Светящаяся палочка фандома BTS (A.R.M.Y.)"
     product.path_to_img = "пока нет"
     product.price = "1000000"
+    product.path_to_img = 'armybomb.jpg'
     session = db_session.create_session()
-    dlt = session.query(Product).first()
-    dlt.path_to_img = 'armybomb.png'
+    session.add(product)
     session.commit()"""
+    app.run(port=8000, host='127.0.0.1')
 
 
 @app.route('/')
